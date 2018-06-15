710be8b565cbe40781f159d3f686a85ca4c5e83e
Liam Marley <marley_liam@hotmail.com>
2018-06-15 16:37:25 +0100

    Added the code used to distribute the reviewing duties, although current screening phase is just myself.
    This splits off an additional file for use in the screening process.

--- Screening/article_screening.R
+++ Screening/article_screening.R
@@ -2,3 +2,10 @@ setwd("~/VP_Meta-Analysis/Screening")
 
 #### Packages ####
 library(metagear)
+
+journal_unscreened <- read.csv("journal_articles.csv")
+
+journal_unscreened <- effort_initialize(journal_unscreened)
+
+journal_unscreened <- effort_distribute(journal_unscreened, reviewers = "Liam", save_split = TRUE)
+
