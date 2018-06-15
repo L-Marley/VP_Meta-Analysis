c1031802f18abdc93199b3299c9720e13fea289f
Liam Marley <marley_liam@hotmail.com>
2018-06-15 18:21:47 +0100

    Commit including my mistake and attempting to see why the abstract screener wasn't working.
    LEd to the generation of an addition effort file, which I've now begun article screening with (190 of 346).

--- Screening/article_screening.R
+++ Screening/article_screening.R
@@ -2,10 +2,14 @@ setwd("~/VP_Meta-Analysis/Screening")
 
 #### Packages ####
 library(metagear)
+library(tidyverse)
 
 journal_unscreened <- read.csv("journal_articles.csv")
 
 journal_unscreened <- effort_initialize(journal_unscreened)
 
+journal_unscreened <- rename(journal_unscreened, Authors = ï..Authors)
+
 journal_unscreened <- effort_distribute(journal_unscreened, reviewers = "Liam", save_split = TRUE)
 
+abstract_screener(file = "effort_Liam1.csv", aReviewer = "Liam", titleColumnName = "Title", abstractColumnName = "Abstract")
