66a7cff8caa45336a895e01d59aabd1c983a6d16
Liam Marley <marley_liam@hotmail.com>
2018-06-18 14:13:19 +0100

    Not realising the dois hasn't exported across I'd done all the screening but coming to retrieve the full text for remaining ones has been made problematic now.
    Future commit will either:
    1) Restart the screening with all of the records together now
    
    2) Manually retrieve the dois and then do the theses and book sections.
    
    Amendment: the most recent iteration of the merging attempts on the script

--- Screening/article_screening.R
+++ Screening/article_screening.R
@@ -4,6 +4,8 @@ setwd("C:/Users/Liam Marley/OneDrive/University Work/MRes/Research Dissertation/
 library(metagear)
 library(tidyverse)
 
+#### Loading and Screening ####
+
 journal_unscreened <- read.csv("journal_articles.csv")
 
 journal_unscreened <- effort_initialize(journal_unscreened)
@@ -13,3 +15,15 @@ journal_unscreened <- rename(journal_unscreened, Authors = ï..Authors)
 journal_unscreened <- effort_distribute(journal_unscreened, reviewers = "Liam", save_split = TRUE)
 
 abstract_screener(file = "effort_Liam1.csv", aReviewer = "Liam", titleColumnName = "Title", abstractColumnName = "Abstract")
+
+# I failed to realise the DOIs hadn't exported properly when exporting from endnote
+# and carried on regardless
+# subsequent script is to add the available DOIs to the abstract screened file
+
+journal_screened <- read_csv("effort_Liam1.csv")
+
+doi_dat <- read_csv("all_journal_dois.csv")
+
+journal_screenedtemp <- full_join(journal_screened, doi_dat)
+
+# merging is a mess, duplicates are everywhere
