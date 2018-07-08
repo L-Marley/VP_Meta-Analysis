3f0adb960aefcc086203ad76eb3ff65cb06892b6
Liam Marley <marley_liam@hotmail.com>
2018-07-08 11:41:14 +0100

    Generalised the approach for calculating missing statistics in general and keeping a record of this.
    Previous files are now essentially redundant with the introduction of the markdown document, and are only retained for posterity.

--- History of calculated p-values.R
+++ History of calculated p-values.R
@@ -2,14 +2,16 @@
 
 # Follow the following format for keeping this log/history
 
-# Publication ID
+#### Publication ID
 # Authors (year)
-# Page number of the test/s
+# Page number of the statistic
+# Calculation
 
 # Exemplar: 
-# 35
+#### Pub_ID: 35
 # Smith and Jones (2009)
 # pp. 15
+# Exact p-values
 
 #### Functions ####
 
@@ -30,4 +32,43 @@ p_from_r <- function(r, df) {
   return(pval)
 }
 
+# Standard deviation of the mean
+
+# If the standard error is provided
+# where se = the standard error, and n = group size
+
+SD_from_SE <- function(se, n) {
+  n <- sqrt(n)
+  sd <- round((se/n), 2)
+  return(sd)
+}
+
 #### History ####
+
+#### Pub_ID: 177 ####
+# Chu et al. (2015)
+# pp. 768
+
+# Standard deviation of the mean for BDI 
+
+# Control Group
+
+SD_from_SE()
+
+# Suicide Attempt
+
+SD_from_SE()
+
+# Standard deviation of the means for delayed task recall
+
+# Field perspective - Control group
+SD_from_SE(.14, 74)
+
+# Observer perspective - Control group
+SD_from_SE(.12, 74)
+
+# Field perspective - Suicide attempt
+SD_from_SE(.35, 17)
+
+# Observer perspective - Suicide attempt
+SD_from_SE(.27, 17)
