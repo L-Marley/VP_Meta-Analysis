2184369816c446b45b066d555c8ef6f4543c17fa
Liam Marley <marley_liam@hotmail.com>
2018-07-09 19:12:59 +0100

    Added a function to calculate a two-sample t-test.
    Begun the process of calculating missing statistics in the markdown document.
    
    Currently done so for studies 379 and 255

--- Coding_Files/Functions for calculating exact p-value.R
+++ Coding_Files/Functions for calculating exact p-value.R
@@ -36,3 +36,40 @@ p_from_r <- function(r, df) {
 # if df aren't specified then it's simply the sample size - 2 (N - 2)
 
 p_from_r(r = .303, df = 68)
+
+#### Standard Deviation of the Mean ####
+
+# If the standard error is provided
+# where se = the standard error, and n = group size
+
+SD_from_SE <- function(se, n) {
+  n <- sqrt(n)
+  sd <- round( (se/n), 2 )
+  return(sd)
+}
+
+#### Independent Samples t-test ####
+
+# m1, m2: the sample means
+# s1, s2: the sample standard deviations
+# n1, n2: the same sizes
+# m0: the null value for the difference in means to be tested for. Default is 0. 
+# equal.variance: whether or not to assume equal variance. Default is FALSE. 
+t_test2 <- function(m1, m2, sd1, sd2, n1, n2, m0=0, equal.variance = FALSE)
+{
+  if(equal.variance == FALSE) 
+  {
+    se <- sqrt( (sd1^2/n1) + (sd2^2/n2) )
+    # welch-satterthwaite df
+    df <- ( (sd1^2/n1 + sd2^2/n2)^2 )/( (sd1^2/n1)^2/(n1-1) + ( sd2^2/n2)^2/(n2-1) )
+  } else
+  {
+    # pooled standard deviation, scaled by the sample sizes
+    se <- sqrt( (1/n1 + 1/n2) * ( (n1-1)*sd1^2 + (n2-1)*sd2^2)/(n1+n2-2) ) 
+    df <- n1+n2-2
+  }      
+  t <- (m1-m2-m0)/se 
+  dat <- c(m1-m2, se, t, df, 2*pt( -abs(t), df) )    
+  names(dat) <- c("Difference of means", "Std Error", "t", "df", "p-value")
+  return(dat) 
+}
